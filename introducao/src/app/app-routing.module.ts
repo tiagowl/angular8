7f2d4c32e1da67f9dab03f0ad9d213c7e5a187a4
= <tiagolandi@egali.local>
2019-11-11 14:14:34 -0300

    Criando o primeiro componente e definindo sua rota

--- introducao/src/app/app-routing.module.ts
+++ introducao/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'meu-componente', component: MeuComponenteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- introducao/src/app/app.module.ts
+++ introducao/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent
   ],
   imports: [
     BrowserModule,
